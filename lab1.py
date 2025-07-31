2862ab5fe736ba2f55dcbfc2a12355a0c29e103b
bhanu-0509 <bhanusinghmar05@gmail.com>
2025-07-31 19:21:55 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,18 +1,21 @@
-marks = input("Enter your marks (0-100): ")
-
-if marks.isdigit():
-    marks = int(marks)
-    if 90 <= marks <= 100:
-        print("A")
-    elif 75 <= marks <= 89:
-        print("B")
-    elif 60 <= marks <= 74:
-        print("C")
-    elif 40 <= marks <= 59:
-        print("D")
-    elif 0 <= marks < 40:
-        print("F")
-    else:
-        print("INVALID INPUT")
-else:
-    print("INVALID INPUT")
+while True:
+    user = input("Press Enter to continue or type 'EXIT' to quit: ")
+    if user == "EXIT":
+        print("Exit successfully.")  # if user enter EXIT then it will exit the program
+        break  # code exit
+    try:
+        marks = int(input("enter the marks: "))
+        if 90 <= marks <= 100: # marks is greater then and equal to 90 and less then and equal to 100 then grade is A
+            print("A")
+        elif 75 <= marks <= 89:
+            print("B")
+        elif 60 <= marks <= 74:
+            print("C")
+        elif 40 <= marks <= 59:
+            print("D")
+        elif 0 <= marks < 40:
+            print("F")
+        else:
+            print("Invalid input")
+    except:
+        print("Invalid input") # if user enter any other value then it will show invalid input
