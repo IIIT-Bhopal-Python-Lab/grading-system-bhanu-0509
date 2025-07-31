45aafafeeb080eeec37de18b75d61d55ca23f255
bhanu-0509 <bhanusinghmar05@gmail.com>
2025-07-31 15:06:34 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -1,3 +1,18 @@
-#Write your code here in python 
-n=int(input("enter a nuber"))
-print(n)
+marks = input("Enter your marks (0-100): ")
+
+if marks.isdigit():
+    marks = int(marks)
+    if 90 <= marks <= 100:
+        print("A")
+    elif 75 <= marks <= 89:
+        print("B")
+    elif 60 <= marks <= 74:
+        print("C")
+    elif 40 <= marks <= 59:
+        print("D")
+    elif 0 <= marks < 40:
+        print("F")
+    else:
+        print("INVALID INPUT")
+else:
+    print("INVALID INPUT")
